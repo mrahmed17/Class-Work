032ecfa4400e21de4c906fd83f6c4e8bf75b427d
Tamim Ahmed Raju <138282465+mrahmed17@users.noreply.github.com>
2024-04-22 16:53:38 +0600

    22.04.2024

---
+++ Core-JAVA/ComputeAreaClass2/src/computeareaclass2/ComputeAreaClass2.java
@@ -0,0 +1,22 @@
+
+package computeareaclass2;
+
+
+public class ComputeAreaClass2 {
+
+   
+    public static void main(String[] args) {
+      
+        int radius;
+        double area;
+        
+        radius=13;
+        
+        area=radius*radius*3.1416;
+        
+        System.out.println(area);
+        
+        
+    }
+    
+}
