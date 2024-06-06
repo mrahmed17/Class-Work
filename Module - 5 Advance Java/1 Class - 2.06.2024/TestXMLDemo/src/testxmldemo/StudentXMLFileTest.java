7bfc84fa632ea49a0aa5f23f493b78edd23dd33c
Md. Raju Ahmed <tamimahmedraju@gmail.com>
2024-06-06 12:43:15 +0600

    XML Reader Test

--- Module - 5 Advance Java/1 Class - 2.06.2024/TestXMLDemo/src/testxmldemo/StudentXMLFileTest.java
+++ Module - 5 Advance Java/1 Class - 2.06.2024/TestXMLDemo/src/testxmldemo/StudentXMLFileTest.java
@@ -1,4 +1,3 @@
-
 package testxmldemo;
 
 import java.util.logging.Level;
@@ -10,30 +9,21 @@ import org.xml.sax.SAXException;
 
 public class StudentXMLFileTest {
 
-
     public static void main(String[] args) {
 
         SAXParserFactory spf = SAXParserFactory.newInstance();
-
         spf.setNamespaceAware(true);
         spf.setValidating(true);
-
-        System.out.println("Parser will " + (spf.isNamespaceAware() ? "" : "not ")
-                + "be namespace aware");
-
-        System.out.println("Parser will " + (spf.isValidating() ? "" : "Not")  +" validate XML");
-
+        System.out.println("Parser will " + (spf.isNamespaceAware() ? "" : "not ") + "be namespace aware");
+        System.out.println("Parser will " + (spf.isValidating() ? "" : "Not") + " validate XML");
         SAXParser parser = null;
 
         try {
-
             parser = spf.newSAXParser();
-
         } catch (ParserConfigurationException | SAXException ex) {
             Logger.getLogger(XMLReader.class.getName()).log(Level.SEVERE, null, ex);
             //System.exit(1); 
         }
-
         System.out.println("Parser object is: " + parser);
     }
 
--- Module - 5 Advance Java/1 Class - 2.06.2024/TestXMLDemo/src/testxmldemo/TestXMLDemo.java
+++ Module - 5 Advance Java/1 Class - 2.06.2024/TestXMLDemo/src/testxmldemo/TestXMLDemo.java
@@ -1,4 +1,4 @@
-package TestRoot;
+package testxmldemo;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
@@ -11,14 +11,14 @@ public class TestXMLDemo {
 
         // Create factory object
         SAXParserFactory saxpf = SAXParserFactory.newInstance();
-        System.out.println("Parser will" + (saxpf.isNamespaceAware() ? "" : "not ") + "be namespace aware");
+        System.out.println("Parser will " + (saxpf.isNamespaceAware() ? "" : "not ") + "be namespace aware");
         System.out.println("Parser will " + (saxpf.isValidating() ? "" : "not ") + "validate XML");
         SAXParser parser = null; // Stores parser reference
+        
         try {
             parser = saxpf.newSAXParser(); // Create parser object
         } catch (ParserConfigurationException | SAXException e) {// Thrown if a parser cannot be created
-
-// that is consistent with the 
+            // that is consistent with the 
             e.printStackTrace(System.err); // configuration in spf
             System.exit(1);
         }
