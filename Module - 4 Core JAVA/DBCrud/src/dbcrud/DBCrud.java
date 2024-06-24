059ec9f5f9ed76e094077bd38bc5b175902a1616
Tamim Ahmed Raju <138282465+mrahmed17@users.noreply.github.com>
2024-06-24 17:44:12 +0600

    JDBC Connection Practice using xampp

---
+++ Module - 4 Core JAVA/DBCrud/src/dbcrud/DBCrud.java
@@ -0,0 +1,11 @@
+
+package dbcrud;
+
+public class DBCrud {
+
+    public static void main(String[] args) {
+       
+        
+    }
+    
+}
---
+++ Module - 4 Core JAVA/DBCrud/src/util/DBUtil.java
@@ -0,0 +1,15 @@
+
+package util;
+
+import java.sql.Connection;
+
+public class DBUtil {
+    
+    private Connection con =null;
+    private String url = "jdbc:mysql://hocalhost:3307/jdbc";
+    private String user= "root";
+    private String password="1234";
+    private String driver="com.mysql.cj.jdbc.Driver";
+    
+    
+}
